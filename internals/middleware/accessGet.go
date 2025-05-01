b86425f595379d5fb60ef01110594e4da5aba90b
mrpradana2 <mrpradana2@gmail.com>
2025-05-01 20:51:17 +0700

    feat: add middleware for authorization for user role in app tickitz

---
+++ internals/middleware/accessGet.go
@@ -0,0 +1,76 @@
+package middleware
+
+import (
+	"net/http"
+	"slices"
+	"tikcitz-app/internals/models"
+	"tikcitz-app/pkg"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (m *Middleware) AcceessGate(allowedRole ...string) func(*gin.Context) {
+	return func(ctx *gin.Context) {
+		// 1. ambil payload/claims dari context gin
+		claims, exist := ctx.Get("Payload")
+		if !exist {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, models.Message{
+				Status: "failed",
+				Msg: "silahkan login terlebih dahulu",
+			})
+			return
+		} 
+		
+		// type assertion claims menjadi pkg.claims
+		userClaims, ok := claims.(*pkg.Claims)
+		if !ok {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, models.Message{
+				Status: "failed",
+				Msg: "identitas login anda rusak, silahkan login kembali",
+			})
+			return
+		}
+
+		// cek role yang ada di claims
+		if !slices.Contains(allowedRole, userClaims.Role) {
+			ctx.AbortWithStatusJSON(http.StatusForbidden, models.Message{
+				Status: "failed",
+				Msg: "Anda tidak dapat mengakses sumber ini",
+			})
+			return
+		}
+		ctx.Next()
+	}
+}
+
+func (m *Middleware) AcceessGateAdmin(ctx *gin.Context) {
+	// 1. ambil payload/claims dari context gin
+	claims, exist := ctx.Get("Payload")
+	if !exist {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, models.Message{
+			Status: "failed",
+			Msg: "silahkan login terlebih dahulu",
+		})
+		return
+	} 
+	
+	// type assertion claims menjadi pkg.claims
+	userClaims, ok := claims.(*pkg.Claims)
+	if !ok {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, models.Message{
+			Status: "failed",
+			Msg: "identitas login anda rusak, silahkan login kembali",
+		})
+		return
+	}
+
+	// cek role yang ada di claims
+	if userClaims.Role != "admin" {
+		ctx.AbortWithStatusJSON(http.StatusForbidden, models.Message{
+			Status: "failed",
+			Msg: "Anda tidak dapat mengakses sumber ini",
+		})
+		return
+	}
+	ctx.Next()
+}
---
+++ internals/middleware/verify.token.go
@@ -0,0 +1,88 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"strings"
+	"tikcitz-app/internals/models"
+	"tikcitz-app/pkg"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Middleware struct{}
+
+func InitMiddleware() *Middleware {
+	return &Middleware{}
+}
+
+func (u *Middleware) VerifyToken(ctx *gin.Context) {
+	// 1. ambil token dari header
+	bearerToken := ctx.GetHeader("Authorization")
+	log.Println("[Bearer TOKEN]", bearerToken)
+	// cek jika bearerToken kosong
+	if bearerToken == "" {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, models.Message{
+			Status: "failed",
+			Msg:    "silahkan login terlebih dahuluu",
+		})
+		return
+	}
+
+	// verifikasi bearer token
+	if !strings.Contains(bearerToken, "Bearer") {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, models.Message{
+			Status: "failed",
+			Msg: "silahkan login terlebih dahulu",
+		})
+		return
+	}
+
+	// 2. pisahkan token dari bearer
+	token := strings.Split(bearerToken, " ")[1]
+
+	if token == "" {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, models.Message{
+			Status: "failed",
+			Msg:    "silahkan login terlebih dahulu",
+		})
+		return
+	}
+
+	// verifikasi token
+	claims := &pkg.Claims{}
+	if err := claims.VerifyToken(token); err != nil {
+		log.Println(err.Error())
+		// if err.Error() == "expired token" || err.Error() == "token has invalid claims: token is expired" {
+		if strings.Contains(err.Error(), "expired") {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, models.Message{
+				Status: "failed",
+				Msg:    "sesi anda berakhir, silahkan login kembali",
+			})
+			return
+		}
+		// format salah
+		if strings.Contains(err.Error(), "malformed") {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, models.Message{
+				Status: "failed",
+				Msg:    "identitas login anda rusak, silahkan login kembali",
+			})
+			return
+		}
+
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, models.Message{
+			Status: "failed",
+			Msg:    "terjadi kesalahan server",
+		})
+		return
+	}
+
+	// ctx.JSON(http.StatusOK, models.Message{
+	// 	Status: "ok",
+	// 	Msg:    "success",
+	// 	Result: claims,
+	// })
+
+	ctx.Set("Payload", claims)
+	ctx.Next()
+}
