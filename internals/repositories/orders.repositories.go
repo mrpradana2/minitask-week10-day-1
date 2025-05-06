30661c746ec8d044991e8460ccf4ca547b1538ac
mrpradana2 <mrpradana2@gmail.com>
2025-05-06 14:40:41 +0700

    fix: build query use query dinamic

--- internals/repositories/orders.repositories.go
+++ internals/repositories/orders.repositories.go
@@ -2,8 +2,9 @@ package repositories
 
 import (
 	"context"
-	"errors"
+	"log"
 	"tikcitz-app/internals/models"
+	"tikcitz-app/internals/utils"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -29,43 +30,46 @@ func (o *OrdersRepository) CreateOrder(ctx context.Context, order models.OrdersS
 	}
 
 	// berikan poin kepada user yang membeli
-	// mengambil nilai point user dari tabel profile
-	querySelectPoint := "SELECT point FROM profiles WHERE user_id = $1"
-	var point int
-	if err := o.db.QueryRow(ctx, querySelectPoint, IdInt).Scan(&point); err != nil {
+	// update point user di table profiles
+	queryAddPointUser := "UPDATE profiles SET point = point + 50 WHERE user_id = $1"
+	if _, err := o.db.Exec(ctx, queryAddPointUser, IdInt); err != nil {
 		return err
 	}
 
-	// menambahkan point +50 untuk setiap kali order
-	point = point + 50
+	// build dinamic query untuk mengambil seat_id dari table seats 
+	querySelectSeats, seats := utils.GetIdTable("seats", "kode", order.Seats)
 
-	// melakukan update point terbaru
-	queryUpdatePoint := "UPDATE profiles SET point = $1 WHERE user_id = $2"
-	if	_, err := o.db.Exec(ctx, queryUpdatePoint, point, IdInt); err != nil {
+	// mengeksekusi query select seat_id yang sudah di build
+	rows, err := o.db.Query(ctx, querySelectSeats, seats...)
+	if err != nil {
+		log.Println(err.Error())
+		return err
+	}
+	
+	defer rows.Close()
+
+	idSeats := []any{orderId}
+	for rows.Next() {
+		var idSeat int
+		if err := rows.Scan(&idSeat); err != nil {
+			return err
+		}
+		idSeats = append(idSeats, idSeat)
+	}
+
+	log.Println(idSeats...)
+
+	// menambahkan order_id dan order seat ke table asosiasi order_seats
+	// melakukan build untuk query insert order_seats
+	queryInsertOrderSeats := utils.InsertTableAssoc("order_seats", "order_id", "seat_id", idSeats)
+
+	// mengeksekusi query insert order_seats yang sudah di build
+	log.Println("Query Insert OrderSeats", queryInsertOrderSeats)
+	log.Println("idSeats", idSeats)
+	if _, err := o.db.Exec(ctx, queryInsertOrderSeats, idSeats...); err != nil {
 		return err
 	}
 
-	// melakukan looping berdasarkan inputan seats
-	for _, seatStr := range order.Seats {
-
-		// mengambil seat id pada table seats berdasarkan kode seat dan schedule id 
-		querySelectIdSeat := "SELECT id FROM seats WHERE kode = $1"
-		var seatId int
-
-		if err := o.db.QueryRow(ctx, querySelectIdSeat, seatStr).Scan(&seatId); err != nil {
-			return err
-		} 
-
-		if seatId == 0 {
-			return errors.New("kursi tidak tersedia")
-		}
-
-		// menambahkan order_id dan seat_id pada tabel asosiasi order_seats 
-		queryInsertOrderSeats := "INSERT INTO order_seats (order_id, seat_id) VALUES ($1, $2)"
-		if _, err := o.db.Exec(ctx, queryInsertOrderSeats, orderId, seatId); err != nil {
-			return err
-		}
-	}
 	return nil
 }
 
@@ -73,7 +77,7 @@ func (o *OrdersRepository) CreateOrder(ctx context.Context, order models.OrdersS
 func (o *OrdersRepository) GetOrderHistory(ctx context.Context, IdInt int) ([]models.OrdersStr, error) {
 
 	// mengambil data dari beberapa tabel yang di joinkan (orders, payment_methode, movies, cinemas, order_seats, dan seats) berdasarkan user_id
-	query := "select o.id, m.title, c.image_path, o.date, o.time, o.total_price, o.paid, array_agg(s2.kode) from orders o join schedule s on o.schedule_id = s.id join payment_methode pm on pm.id = o.payment_methode_id join cinemas c on s.cinema_id = c.id join movies m on m.id = s.movie_id join users u on u.id = o.user_id join order_seats os on os.order_id = o.id join seats s2 on s2.id = os.seat_id where u.id = $1 group by o.id, c.image_path, m.title, o.date, o.time, o.total_price, o.paid"
+	query := "select o.id, m.title, c.image_path, o.date, o.time, o.total_price, o.paid, array_agg(s2.kode) from orders o join schedule s on o.schedule_id = s.id join payment_methode pm on pm.id = o.payment_methode_id join cinemas c on s.cinema_id = c.id join movies m on m.id = s.movie_id join users u on u.id = o.user_id join order_seats os on os.order_id = o.id join seats s2 on s2.id = os.seat_id where u.id = $1 group by o.id, c.image_path, m.title, o.date, o.time, o.total_price, o.paid order by o.create_at desc"
 	values := []any{IdInt}
 	rows, err := o.db.Query(ctx, query, values...)
 	if err != nil {
