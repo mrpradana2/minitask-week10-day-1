1a38bc0e8da0b3e0539e06b5247b75e237ddf28a
mrpradana2 <mrpradana2@gmail.com>
2025-06-14 23:55:47 +0700

    feat: added validation for add movie, edit movie, and order movie form

--- internals/models/movies.models.go
+++ internals/models/movies.models.go
@@ -9,16 +9,16 @@ import (
 
 type MoviesStruct struct {
 	Id				int	        `json:"id,omitempty"`
-	Title           string      `json:"title" form:"title"`
-	Release_date    time.Time   `json:"release_date" form:"release_date"`
-	Overview        string      `json:"overview" form:"overview"`
-	Duration        int         `json:"duration" form:"duration"`
-	Director_name   string      `json:"director_name" form:"director_name"`
-	Genres          []string    `json:"genres" form:"genres"`
-	Casts           []string    `json:"casts" form:"casts"`
+	Title           string      `json:"title" form:"title" binding:"required"`
+	Release_date    time.Time   `json:"release_date" form:"release_date" binding:"required"`
+	Overview        string      `json:"overview" form:"overview" binding:"required"`
+	Duration        int         `json:"duration" form:"duration" binding:"required"`
+	Director_name   string      `json:"director_name" form:"director_name" binding:"required"`
+	Genres          []string    `json:"genres" form:"genres" binding:"required"`
+	Casts           []string    `json:"casts" form:"casts" binding:"required"`
 	Image_movie     string      `json:"image_movie"`
 	TotalSales      int         `json:"total_sales,omitempty"`
-	Image_path      *multipart.FileHeader `json:"image_path,omitempty" form:"image_path,omitempty"` 
+	Image_path      *multipart.FileHeader `json:"image_path,omitempty" form:"image_path,omitempty" binding:"required"` 
 	Cinema_ids      []int       `json:"cinema_ids,omitempty" form:"cinema_ids,omitempty"`
 	Movie_id        int         `json:"movie_id,omitempty" form:"movie_id,omitempty"`
 	Location        string 	    `json:"location,omitempty" form:"location,omitempty"`
--- internals/models/orders.models.go
+++ internals/models/orders.models.go
@@ -27,18 +27,18 @@ import (
 type OrdersStr struct {
 	Id *int `json:"id,omitempty" form:"id,omitempty"`
 	UserId int `json:"user_id,omitempty" form:"user_id,omitempty"`
-	ScheduleId int `json:"schedule_id,omitempty" form:"schedule_id"`
-	PaymentMethodeId int `json:"payment_methode_id,omitempty" form:"payment_methode_id"`
-	Date time.Time `json:"date" form:"date"`
-	Time time.Time `json:"time" form:"time"`
-	TotalPrice int `json:"total_price" form:"total_price"`
-	FullName string `json:"full_name,omitempty" form:"full_name"`
-	Email string `json:"email,omitempty" form:"email"`
-	PhoneNumber string `json:"phone_number,omitempty" form:"phone_number"`
-	Paid bool `json:"paid" form:"paid"`
+	ScheduleId int `json:"schedule_id,omitempty" form:"schedule_id" binding:"required"`
+	PaymentMethodeId int `json:"payment_methode_id,omitempty" form:"payment_methode_id" binding:"required"`
+	Date time.Time `json:"date" form:"date" binding:"required"`
+	Time time.Time `json:"time" form:"time" binding:"required"`
+	TotalPrice int `json:"total_price" form:"total_price" binding:"required"`
+	FullName string `json:"full_name,omitempty" form:"full_name" binding:"required"`
+	Email string `json:"email,omitempty" form:"email" binding:"required,email"`
+	PhoneNumber string `json:"phone_number,omitempty" form:"phone_number" binding:"required"`
+	Paid bool `json:"paid" form:"paid" binding:"required"`
 	OrderId int `json:"order_id,omitempty" form:"order_id"`
 	SeatId int `json:"seat_id,omitempty" form:"seat_id,omitempty"`
-	Seats []string `json:"seats" form:"seats"`
+	Seats []string `json:"seats" form:"seats" binding:"required"`
 	ImagePath string `json:"image_cinema_path"`
 	Title string `json:"title"`
 }
